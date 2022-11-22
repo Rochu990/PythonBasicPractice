cf7df05376cc457d5cfc08e57d2153cc08682859
unknown <Przemek Rostek>
2022-11-22 14:22:24 +0100

    Nowe pliki z operatorami

---
+++ basics/02 operators/concatenation operator.py
@@ -0,0 +1,9 @@
+
+strData = "Hello " + "World" + "!"
+print(strData)
+print(strData + " and hello again!")
+
+listData  = [1,2,3,4,5,6]
+newComponents = [7,8,9,10]
+
+print(listData + newComponents)
