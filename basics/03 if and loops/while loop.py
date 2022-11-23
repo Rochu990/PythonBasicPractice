6e31844b92d5c20b6f5fdc003770681d1480fbab
unknown <Przemek Rostek>
2022-11-23 17:46:21 +0100

    Praktyka pętla while

---
+++ basics/03 if and loops/while loop.py
@@ -0,0 +1,29 @@
+
+# number = 5
+
+# while number > 0:
+#     print(number)
+#     number -= 1
+
+# number = 1
+
+# while number < 6:
+#     print(number)
+#     number += 1
+
+# num = 0
+
+# while True:
+#     print("Wprowadź liczbe lub exit aby zakończyć program")
+#     strData = input()
+#     if strData == "exit": break
+#     num += int(strData)
+#     print("Wartość po dodaniu liczby: " + str(num))
+
+number = 3
+
+while number > 0:
+    print(number)
+    number -= 1
+else:
+    print("number po pętli: " + str(number))
