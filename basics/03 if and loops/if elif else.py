ab7cfb0859f655cbfb3870332bcc28f393cb5ccc
unknown <Przemek Rostek>
2022-11-23 17:19:58 +0100

    Praktyka if, elif, else

---
+++ basics/03 if and loops/if elif else.py
@@ -0,0 +1,36 @@
+
+# num = 5
+
+# if num >= 3:
+#     print("num większe lub równe")
+#     print("oraz !")
+
+# if num == 5:
+#     print("5 równe jest 5")
+
+# num = 0
+
+# if num == 1:
+#     print("num równe jest 1")
+# elif num == 2:
+#     print("num równe jest 2")
+# elif num == 5:
+#     print("num równe jest 5")
+# elif num == 10:
+#     print("num równe jest 10")
+# elif num >= 11:
+#     print("num większe lub równe 11")
+# else:
+#     print("Brak ")
+
+# print("dalsza część kodu")
+
+if 5 > 1: print("5 jest większe od 1")
+
+if 10 > 2:
+    print("10 jest większe od 2")
+    if 4 > 1:
+        print("4 jest większe od 1")
+        if 3 > 2:
+            print("3 jest większe od 2")
+        print("4 większe od 1 c.d.")
